4f7c71886fddfd14105f58d3f2f770fc91c20a00
Jorge Gonzalez <jorge.1014@gmail.com>
2023-10-09 19:58:01 -0700

    updated the models and some routes

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -2,11 +2,23 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    // * Id has: a interger,  allowNull is false, PrimaryKey true, and autoIncrement true
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // * category_name has: a string,  and allowNull is false
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
